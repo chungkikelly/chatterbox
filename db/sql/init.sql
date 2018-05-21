a4e1b3fb2c57e31a3f54e986c3faddcd679731f7
Kelly Chung <chungkikelly@gmail.com>
2018-05-21 02:36:28 -0700

    add initial SQL queries to define schema

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,5 +1,32 @@
 USE challenge;
 
-CREATE TABLE test(col VARCHAR(10));
+CREATE TABLE users(
+  ID integer NOT NULL AUTO_INCREMENT,
+  Username varchar(255) NOT NULL UNIQUE,
+  created_at datetime NOT NULL,
+  updated_at datetime NOT NULL,
+  PRIMARY KEY (ID)
+);
 
-INSERT INTO test(col) VALUES('ok');
+CREATE TABLE channels(
+  ID integer NOT NULL AUTO_INCREMENT,
+  Username varchar(255) NOT NULL,
+  created_at datetime NOT NULL,
+  updated_at datetime NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE messages(
+  ID integer NOT NULL AUTO_INCREMENT,
+  body varchar(255) NOT NULL,
+  author_id integer NOT NULL,
+  channel_id integer NOT NULL,
+  created_at datetime NOT NULL,
+  updated_at datetime NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (author_id) REFERENCES users(ID),
+  FOREIGN KEY (channel_ID) REFERENCES channels(ID)
+);
+
+CREATE INDEX (message_user) ON messages (author_id);
+CREATE INDEX (message_channel) ON messages (channel_id);
